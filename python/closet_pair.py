c5a5f3ac1e38228e8772a392305690d73d7b0b50
mu-majid <muhammed.m@trufla.com>
2020-02-22 18:14:45 +0200

    WIP closet pair algorithm with divide and conquer paradigm

---
+++ python/closet_pair.py
@@ -0,0 +1,17 @@
+"""
+Divide and Conquer Algorithm for determinning the closet pair of point in plane R^2
+
+Assume : No Ties in either x or y coordinates.
+"""
+
+def calc_dist_brute():
+  pass
+
+def euclidean_dist(p_1, p_2):
+  pass
+
+def closet_split_pair():
+  pass
+
+def closet_pair():
+  pass
